dfa5a05dc33d8a5ee9edf9861e242ac6aed549eb
Ana Damasceno <107864553+anadamascenos@users.noreply.github.com>
2023-10-30 00:26:11 -0300

    Add files via upload

---
+++ rest-api-estacionamento/rest-api-estacionamento/src/main/java/com/example/restapiestacionamento/RestApiEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapiestacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiEstacionamentoApplication.class, args);
+	}
+
+}
---
+++ rest-api-estacionamento/rest-api-estacionamento/src/test/java/com/example/restapiestacionamento/RestApiEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapiestacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
